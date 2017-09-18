da9903f8ca22658f8ac93d29ae4cdfce9d6a99b5
Wilson Phoong <wphoong@gmail.com>
2017-09-17 20:14:58 -0700

    fixed server.js, required config/db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
